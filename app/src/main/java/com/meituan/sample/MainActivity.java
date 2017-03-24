84f9b0139431207b249a3e391a9daefce42a1acd
张梦 <zhangmeng11@meituan.com>
2017-03-24 11:18:00 +0800

    clean demo code

--- app/src/main/java/com/meituan/sample/MainActivity.java
+++ app/src/main/java/com/meituan/sample/MainActivity.java
@@ -1,22 +1,40 @@
 package com.meituan.sample;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.meituan.Hll;
+import com.meituan.sample.robusttest.other.Hll;
 import com.meituan.robust.Patch;
 import com.meituan.robust.PatchExecutor;
 import com.meituan.robust.RobustCallBack;
+import com.meituan.sample.robusttest.ImageQualityUtil;
+import com.meituan.sample.robusttest.NoField;
+import com.meituan.sample.robusttest.SampleClass;
+import com.meituan.sample.robusttest.State;
+import com.meituan.sample.robusttest.Super;
 
-import java.lang.reflect.Constructor;
+/**
+ *
+ * For users of Robust you may only to use MainActivity or SecondActivity,other classes are used for test.<br>
+ *<br>
+ * If you just want to use Robust ,we recommend you just focus on MainActivity SecondActivity and PatchManipulateImp.Especially three buttons in MainActivity<br>
+ *<br>
+ * in the MainActivity have three buttons; "SHOW TEXT " Button will change the text in the MainActivity,you can patch the show text.<br>
+ *<br>
+ * "PATCH" button will load the patch ,the patch path can be configured in PatchManipulateImp.<br>
+ *<br>
+ * "JUMP_SECOND_ACTIVITY" button will jump to the second ACTIVITY,so you can patch a Activity.<br>
+ *<br>
+ * Attention to this ,We recommend that one patch is just for one built apk ,because every  built apk has its unique mapping.txt and resource id<br>
+ *
+ *@author mivanzhang
+ */
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
 
     Hll hll = new Hll(false);
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,10 +51,31 @@ public class MainActivity extends AppCompatActivity {
         button = (Button) findViewById(R.id.button);
         textView = (TextView) findViewById(R.id.textView);
         state = new State<>(hll);
+        Button patch = (Button) findViewById(R.id.patch);
+        //beigin to patch
+        patch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new PatchExecutor(getApplicationContext(), new PatchManipulateImp(),  new Callback()).start();
+
+            }
+        });
+
+        findViewById(R.id.jump_second_activity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+                Log.d("robusttest", (new NoField()).toString());
+                Log.d("robusttest", ImageQualityUtil.getDefaultSize("asdasdasd"));
+                SampleClass sampleClass=new SampleClass();
+                sampleClass.multiple(-1);
+            }
+        });
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //除了get方法有拦截之外，其他的几个方法没有做拦截，可能是处理get set方法的误杀
                 Toast.makeText(MainActivity.this, "arrived in ", Toast.LENGTH_SHORT).show();
                 state.setIndex(hll, 1, 1l, new Object());
                 Log.d("robust", "state.get()  " + state.get().toString());
@@ -47,36 +85,14 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("robust", "patch result after:" + s.protextedMethod());
                 textView.setText(s.getText());
                 s.getinstance();
-//                Log.d("robust", "s.getinstance(new Super())==  :    " + s.getinstance(new Super()));
             }
         });
-        Button patch = (Button) findViewById(R.id.patch);
-        patch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                PatchUtils.applyPatch(MainActivity.this, "group", "1.0", "channel", 1233445l, "qwertyuiop1", "QQQQQQQQQQQ");
-                new PatchExecutor(getApplicationContext(), new PatchManipulateImp(),  new Callback()).start();
-
-            }
-        });
-
+        //test situation,
         try {
             ImageQualityUtil.loadImage(null, null, null, 1, null);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        findViewById(R.id.jump_second_activity).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = (Intent) invokeReflectConstruct("android.content.Intent", new Object[]{MainActivity.this, SecondActivity.class}, new Class[]{Context.class, Class.class});
-                startActivity(intent);
-                Log.d("robusttest", (new NoField()).toString());
-                Log.d("robusttest", ImageQualityUtil.getDefaultSize("asdasdasd"));
-                SampleClass sampleClass=new SampleClass();
-                sampleClass.multiple(-1);
-            }
-        });
-
     }
     //patch  data report
     class Callback implements RobustCallBack {
@@ -108,18 +124,5 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-    public static Object invokeReflectConstruct(String className, Object[] parameter, Class[] args) {
-        try {
-            Class clazz = Class.forName(className);
-            Constructor constructor = clazz.getDeclaredConstructor(args);
-            constructor.setAccessible(true);
-            return constructor.newInstance(parameter);
-        } catch (Exception e) {
-            Log.d("robust", "invokeReflectConstruct construct error " + className + "   parameter   " + parameter);
-            e.printStackTrace();
-        }
-        throw new RuntimeException("invokeReflectConstruct error ");
-    }
 
 }
--- app/src/main/java/com/meituan/sample/PatchManipulateImp.java
+++ app/src/main/java/com/meituan/sample/PatchManipulateImp.java
@@ -18,10 +18,12 @@ public class PatchManipulateImp extends PatchManipulate {
     @Override
     protected List<Patch> fetchPatchList(Context context) {
         //将app自己的robustApkHash上报给服务端，服务端根据robustApkHash来区分每一次apk build来给app下发补丁
+        //apkhash is the unique identifier for  apk,so you cannnot patch wrong apk.
         //String robustApkHash = RobustApkHashUtils.readRobustApkHash(context);
         Patch patch = new Patch();
         patch.setName("123");
-        patch.setLocalPath(Environment.getExternalStorageDirectory().getPath()+ File.separator+"robust"+File.separator + "patch.dex");
+        //we recommend LocalPath store the origin patch.jar which may be encrypted,while TempPath is the true runnable jar
+        patch.setLocalPath(Environment.getExternalStorageDirectory().getPath()+ File.separator+"robust"+File.separator + "patch.jar");
         patch.setTempPath(Environment.getExternalStorageDirectory().getPath()+ File.separator+"robust"+File.separator + "patch");
         patch.setPatchesInfoImplClassFullName("com.meituan.robust.patch.PatchesInfoImpl");
         List  patches = new ArrayList<Patch>();
