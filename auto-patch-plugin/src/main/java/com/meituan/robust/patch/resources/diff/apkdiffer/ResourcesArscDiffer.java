8ffa478bf198cf0cd70d00eab509188346bc5871
hedex <hedexmail@gmail.com>
2017-06-08 14:06:18 +0800

    add differs, assets, android manifest ,lib ,dex ,res,resources arsc ,metainf,apk..
    Signed-off-by: hedex <hedexmail@gmail.com>

---
+++ auto-patch-plugin/src/main/java/com/meituan/robust/patch/resources/diff/apkdiffer/ResourcesArscDiffer.java
@@ -0,0 +1,52 @@
+package com.meituan.robust.patch.resources.diff.apkdiffer;
+
+import com.meituan.robust.patch.resources.APKStructure;
+import com.meituan.robust.patch.resources.config.RobustResourceConfig;
+import com.meituan.robust.patch.resources.diff.data.ResourcesArscDiffData;
+
+import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.regex.Pattern;
+
+/**
+ * Created by hedingxu on 17/5/31.
+ */
+
+public class ResourcesArscDiffer extends BaseDiffer {
+
+    public ResourcesArscDiffer(RobustResourceConfig config) {
+        super(config);
+        diffData = new ResourcesArscDiffData();
+        Pattern pattern = Pattern.compile(APKStructure.ResourcesArsc_Type);
+        HashSet<Pattern> patterns = new HashSet<>();
+        patterns.add(pattern);
+        includePatterns = patterns;
+        excludePatterns = null;
+        apkResourceType = APKStructure.ResourcesArsc_Type;
+    }
+
+    @Override
+    public boolean diffNewFile(Path newFilePath) {
+        if (!isNeed(newFilePath)) {
+            return false;
+        }
+
+        return super.diffNewFile(newFilePath);
+    }
+
+    @Override
+    public boolean diffOldFile(Path oldFilePath) {
+        if (!isNeed(oldFilePath)) {
+            return false;
+        }
+
+        return super.diffOldFile(oldFilePath);
+    }
+
+    @Override
+    protected boolean isNeed(Path filePath) {
+        return true;
+    }
+
+
+}
