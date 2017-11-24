43a3d51e1ec7681d11b22137b5af6706f9185e7e
hedex <hedexmail@gmail.com>
2017-11-25 02:53:08 +0800

    clean some classes
    Signed-off-by: hedex <hedexmail@gmail.com>

--- app/src/main/java/com/meituan/sample/robusttest/ConcreateClass.java
+++ app/src/main/java/com/meituan/sample/robusttest/ConcreateClass.java
@@ -1,17 +1,17 @@
-package com.meituan.sample.robusttest;
-
-/**
- * Created by mivanzhang on 16/11/10.
- */
-
-public class ConcreateClass extends AbstractClass {
-    @Override
-    public void abstractMethodA() {
-
-    }
-
-    @Override
-    public String abstractMethodB() {
-        return null;
-    }
-}
+//package com.meituan.sample.robusttest;
+//
+///**
+// * Created by mivanzhang on 16/11/10.
+// */
+//
+//public class ConcreateClass extends AbstractClass {
+//    @Override
+//    public void abstractMethodA() {
+//
+//    }
+//
+//    @Override
+//    public String abstractMethodB() {
+//        return null;
+//    }
+//}
--- app/src/main/java/com/meituan/sample/robusttest/JustTest.java
+++ app/src/main/java/com/meituan/sample/robusttest/JustTest.java
@@ -1,7 +1,7 @@
-package com.meituan.sample.robusttest;
-
-/**
- * Created by mivanzhang on 16/10/21.
- */
-public class JustTest {
-}
+//package com.meituan.sample.robusttest;
+//
+///**
+// * Created by mivanzhang on 16/10/21.
+// */
+//public class JustTest {
+//}
