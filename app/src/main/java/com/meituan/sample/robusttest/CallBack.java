d5d3c07fe94dfc5d9e7d5b162a1752603325ff15
hedex <hedexmail@gmail.com>
2017-11-25 02:54:45 +0800

    delete call back interface , it is not used
    Signed-off-by: hedex <hedexmail@gmail.com>

--- app/src/main/java/com/meituan/sample/robusttest/CallBack.java
+++ app/src/main/java/com/meituan/sample/robusttest/CallBack.java
@@ -1,9 +1,9 @@
-package com.meituan.sample.robusttest;
-
-/**
- * Created by mivanzhang on 16/12/30.
- */
-
-public interface CallBack {
-    void callBack();
-}
+//package com.meituan.sample.robusttest;
+//
+///**
+// * Created by mivanzhang on 16/12/30.
+// */
+//
+//public interface CallBack {
+//    void callBack();
+//}
