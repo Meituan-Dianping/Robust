dfd1ada6cc60f2adc4af3f9b112960e49fbf17eb
hedex <hedexmail@gmail.com>
2017-11-25 02:49:35 +0800

    clean NoField class
    Signed-off-by: hedex <hedexmail@gmail.com>

--- app/src/main/java/com/meituan/sample/robusttest/NoField.java
+++ app/src/main/java/com/meituan/sample/robusttest/NoField.java
@@ -4,25 +4,4 @@ package com.meituan.sample.robusttest;
  * Created by mivanzhang on 16/10/21.
  */
 public class NoField {
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
-    public String toString() {
-
-        return super.toString();
-    }
-
-    private NoField getName() {
-        this.hashCode();
-        JustTest s = new JustTest();
-        s.test(this);
-        return StaticInner.instance;
-    }
-
-    static class StaticInner {
-        static NoField instance = new NoField();
-    }
 }
