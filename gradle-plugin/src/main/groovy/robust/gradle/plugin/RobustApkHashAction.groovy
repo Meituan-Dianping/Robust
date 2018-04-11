7a9572f48943e4d8127e7b6d94328daf1605fdc6
hedex <hedexmail@gmail.com>
2018-04-11 16:01:49 +0800

    fix gradle 3.1 bug
    Signed-off-by: hedex <hedexmail@gmail.com>

--- gradle-plugin/src/main/groovy/robust/gradle/plugin/RobustApkHashAction.groovy
+++ gradle-plugin/src/main/groovy/robust/gradle/plugin/RobustApkHashAction.groovy
@@ -204,11 +204,32 @@ class RobustApkHashAction implements Action<Project> {
 
     public static boolean isGradlePlugin300orAbove() {
         try {
-            String gradlePluginVersion = com.android.builder.Version.ANDROID_GRADLE_PLUGIN_VERSION
+            String gradlePluginVersion = getGradlePluginVersion()
             return gradlePluginVersion.compareTo("3.0.0") >= 0;
         } catch (Throwable throwable) {
 
         }
         return false;
+    }
+
+    static String getGradlePluginVersion() {
+        String version = null
+        try {
+            def clazz = Class.forName("com.android.builder.Version")
+            def field = clazz.getDeclaredField("ANDROID_GRADLE_PLUGIN_VERSION")
+            field.setAccessible(true)
+            version = field.get(null)
+        } catch (Exception ignore) {
+        }
+        if (version == null) {
+            try {
+                def clazz = Class.forName("com.android.builder.model.Version")
+                def field = clazz.getDeclaredField("ANDROID_GRADLE_PLUGIN_VERSION")
+                field.setAccessible(true)
+                version = field.get(null)
+            } catch (Exception ignore) {
+            }
+        }
+        return version
     }
 }
